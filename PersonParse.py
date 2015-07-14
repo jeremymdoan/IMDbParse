8e9d9861248fb2211808c4ea76826aa22c8bb151
Jeremy Doan <jeremymdoan@gmail.com>
2015-07-14 15:46:35 -0600

    more work

--- PersonParse.py
+++ PersonParse.py
@@ -12,14 +12,23 @@ def readPersonLines(filename):
         for line in file:
             if line == '\n' or re.search(r'^----.*', line):
                 continue
-            elif count == 50:
+            elif count == 100:
                 break
             else:
                 yield line
                 count += 1
 
-pat = re.compile(r'^([^\t]*)\t+(.*)\s\((\d+)?\/?I*\)\s(.*)$')
-groups = (pat.match(line) for line in readPersonLines('actors.list'))
-tuples = (g.groups() for g in groups if g)
+def extractPersonData(line):
+    pat = re.compile(r'^([^\t]*)\t+(.*)\s\((\d+)?\/?I*\)\s(.*)$')
+    g = pat.match(line)
+    output = [g.group(1), g.group(2), g.group(3)]
+    pat2 = ('(^\{.*\})', '(\[.*\])','(\(T?V\))', '\((archive\sfootage|voice)\)')
+    for p in pat2:
+        g2 = re.search(p, g.group(4))
+        thing = g2.group(1) if g2 else ''
+        output.append(thing)
+    return '~'.join(output)
+
+tuples = list(extractPersonData(line) for line in readPersonLines('actors.list'))
 for t in tuples:
     print(t)
