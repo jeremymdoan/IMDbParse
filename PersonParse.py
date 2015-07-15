07410d9a38c6fb29fd3ea7a1d3c341b64ae8adfe
Jeremy Doan <jeremymdoan@gmail.com>
2015-07-15 14:22:51 -0600

    More updates
    Not quite working yet. Still have some encoding issues I need to deal with.

--- PersonParse.py
+++ PersonParse.py
@@ -1,6 +1,8 @@
 __author__ = 'jeremy.doan'
 
 import re
+from uuid import uuid4
+import os
 
 def readPersonLines(filename):
     count = 0
@@ -12,23 +14,61 @@ def readPersonLines(filename):
         for line in file:
             if line == '\n' or re.search(r'^----.*', line):
                 continue
-            elif count == 100:
-                break
+            #elif count == 1000:
+            #    break
             else:
                 yield line
                 count += 1
 
 def extractPersonData(line):
-    pat = re.compile(r'^([^\t]*)\t+(.*)\s\((\d+)?\/?I*\)\s(.*)$')
+    pat = re.compile(r'^([^\t]*)\t+(.*)\s\((\d{4})\S*\)\s(.*)$')
     g = pat.match(line)
-    output = [g.group(1), g.group(2), g.group(3)]
+    global name
+    try:
+        name = getName(g.group(1)) if not g.group(1) == '' else name
+    except AttributeError:
+        return '!&8%$#' + line
+    output = [name, g.group(2), g.group(3)]
     pat2 = ('(^\{.*\})', '(\[.*\])','(\(T?V\))', '\((archive\sfootage|voice)\)')
     for p in pat2:
         g2 = re.search(p, g.group(4))
         thing = g2.group(1) if g2 else ''
         output.append(thing)
-    return '~'.join(output)
+    try:
+        return re.sub("[\[\]\{\}]", "", '~'.join(output))
+    except TypeError:
+        return '!&8%$#' + line
 
-tuples = list(extractPersonData(line) for line in readPersonLines('actors.list'))
-for t in tuples:
-    print(t)
+def getName(name):
+    g = re.search('(^.*),\s(.*)', name)
+    name = g.group(2) + " " + g.group(1) if g else name
+    g = re.search('(^.*)\s(\(\w*)\s(.*$)', name)
+    name = g.group(1) + " " + g.group(3) + " " + g.group(2) if g else name
+    return str(uuid4()).replace("-", "")[:8] + "~" + name
+
+def run(filename):
+    lines = (extractPersonData(line) for line in readPersonLines(filename))
+    output_file = os.path.splitext(os.path.basename(filename))[0] + '.txt'
+    for f in [output_file, 'error_lines.txt']:
+        if os.path.exists(f):
+            os.remove(f)
+    with open(output_file, 'w') as out_file:
+        with open('error_lines.txt', 'w') as error_file:
+            counter = 0
+            for line in lines:
+                if  line[:6] == '!&8%$#':
+                    error_file.write(line + '\n')
+                else:
+                    out_file.write(line + '\n')
+                counter += 1
+                """if counter%10000 == 0:
+                    print("flushing")
+                    out_file.flush()
+                    error_file.flush()"""
+
+if __name__=="__main__":
+    import sys
+    if len(sys.argv) > 1:
+        run(sys.argv[1])
+    else:
+        run('actors.list')
